c251b90be7e8111cd63f854689198d783d3931a7
OlexTS <sashokseven@gmail.com>
2023-01-09 21:12:24 +0200

    added total and percentage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,17 +14,31 @@ class App extends Component {
     });
   };
 
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((acc, option) => acc + option, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const {good} = this.state;
+    return Math.round((good / this.countTotalFeedback()) * 100 || 0);
+  };
+
   render() {
-    const {good, neutral, bad} = this.state
+    const { good, neutral, bad } = this.state;
     return (
       <div>
         <h1>Please leave feedback</h1>
-        <FeedbackOptions options={ Object.keys(this.state) } onLeaveFeedback={this.createFeedback } />
+        <FeedbackOptions
+          options={Object.keys(this.state)}
+          onLeaveFeedback={this.createFeedback}
+        />
         <h1>Statistic</h1>
         <ul>
           <li>Good: {good}</li>
           <li>Neutral: {neutral} </li>
           <li>Bad: {bad}</li>
+          <li>Total: {this.countTotalFeedback()}</li>
+          <li>Positive feedback: {this.countPositiveFeedbackPercentage()} </li>
         </ul>
       </div>
     );
