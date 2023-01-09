862f236eb6b0f44566c2a14dd370defb4b031a26
OlexTS <sashokseven@gmail.com>
2023-01-09 20:42:14 +0200

    created FeedbackOptions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import FeedbackOptions from './FeedbackOptions';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  createFeedback = option => {
+    this.setState(prevState => {
+      return { [option]: prevState[option] + 1 };
+    });
+  };
+
+  render() {
+    const {good, neutral, bad} = this.state
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+        <FeedbackOptions options={ Object.keys(this.state) } onLeaveFeedback={this.createFeedback } />
+        <h1>Statistic</h1>
+        <ul>
+          <li>Good: {good}</li>
+          <li>Neutral: {neutral} </li>
+          <li>Bad: {bad}</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
